fb24eaf2b059e3c88537843a09ec1a7f529cb000
Robert Burcea <robert.burcea@gmail.com>
2025-02-02 22:40:51 +0200

    reveal

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -110,7 +110,7 @@ export const initializeProducts = (location: "cantina" | "viva") => {
       category: "ASCUNSE",
       location: "cantina",
       barcode: "888888888",
-      hidden: true,
+      hidden: false,
     },
   ];
 
